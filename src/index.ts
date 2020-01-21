1ee3af7647b2ad2ba480c6569537488c55186774
Vinicius Smanioto <smaniotov@gmail.com>
2020-01-21 01:46:23 -0300

    add main file

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import fastify from 'fastify';
+import dotenv from 'dotenv';
+import 'reflect-metadata';
+import { mongoClient } from './utils/db';
+import initializeApolloServer from './utils/initializeApolloServer';
+
+dotenv.config();
+const app = fastify({ logger: true });
+
+const server = initializeApolloServer();
+
+(async () => {
+  const port = 8080;
+
+  try {
+    await mongoClient.connect();
+    app.register(server.createHandler());
+    await app.listen(port);
+    app.log.info(`Server is listening in port ${port}`);
+  } catch (e) {
+    app.log.error(e);
+    process.exit(1);
+  }
+})();
